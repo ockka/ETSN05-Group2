0b3f67d648aae341443b14e3402eefba7b9bb5c1
Carl Rynegardh <elt12cry@student.lu.se>
2015-09-18 12:21:59 +0200

    Added seekbar

--- app/src/main/java/com/etsn05group2/lampcontroller/ActivityOne.java
+++ app/src/main/java/com/etsn05group2/lampcontroller/ActivityOne.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 
 public class ActivityOne extends Activity {
@@ -13,11 +14,30 @@ public class ActivityOne extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.d("ActivityOne", "" + i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void startActivityTwo(View v) {
         Intent intent = new Intent(this, ActivityTwo.class);
-       startActivity(intent);
+        startActivity(intent);
     }
 
 }
