2de3987a2e8a3c0110bd1584f80a34d978111da0
tfy12fm1 <filip.maansson@gmail.com>
2015-09-24 14:47:02 +0200

    LightBulbActivity added

---
+++ app/src/main/java/com/etsn05group2/lampcontroller/model/LightBulb.java
@@ -0,0 +1,22 @@
+package com.etsn05group2.lampcontroller.model;
+
+/**
+ * Created by Filip on 2015-09-24.
+ */
+public class LightBulb extends Device {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getMacAdress() {
+        return null;
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+}
