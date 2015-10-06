7adbc29593873b3afef1fc528f815711a775f445
Carl Rynegardh <elt12cry@student.lu.se>
2015-10-06 11:45:35 +0200

    private date/time methods added, not yet used by public methods

--- app/src/main/java/com/etsn05group2/lampcontroller/network/NetworkManager.java
+++ app/src/main/java/com/etsn05group2/lampcontroller/network/NetworkManager.java
@@ -8,6 +8,8 @@ import com.etsn05group2.lampcontroller.network.data.DeviceData;
 import com.etsn05group2.lampcontroller.network.data.DeviceStatus;
 import com.etsn05group2.lampcontroller.network.data.ToggledStateResponse;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,8 @@ import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+
+import java.util.Calendar;
 
 /**
  * Created by Niklas on 2015-09-22.
@@ -109,12 +113,18 @@ public class NetworkManager {
         };
     }
 
+
+
+
+
+
+
     static private Callback<List<DataAboutDevice>> allDevicesCall(){
         Callback<List<DataAboutDevice>> call = new Callback<List<DataAboutDevice>>(){
 
             @Override
             public void success(List<DataAboutDevice> dataAboutDevices, Response response) {
-                Log.d("DEN HÄMTAR SKIT", "");
+                Log.d("Succes", "");
                 detectedDevices = dataAboutDevices;
             }
 
@@ -124,5 +134,60 @@ public class NetworkManager {
         return call;
     }
 
+    static private String currentTime(){
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+        String time = (df.format(calendar.getTime()));
+        return time;
+    }
+
+    static private String previousTime() {
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DATE);
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+        int seconds = calendar.get(Calendar.SECOND);
+
+        minutes = (minutes - 10) % 60;
+        if (minutes < 0) {
+            minutes += 60;
+            hours--;
+        }
+        if (hours < 0) {
+            hours += 24;
+            hours--;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(year);
+        sb.append("-");
+        if (month < 10) {
+            sb.append("0");
+        }
+        sb.append(month);
+        sb.append("-");
+        if (day < 10) {
+            sb.append("0");
+        }
+        sb.append(day);
+        sb.append(" ");
+        if (hours < 10) {
+            sb.append("0");
+        }
+        sb.append(hours);
+        sb.append(":");
+        if (minutes < 10) {
+            sb.append("0");
+        }
+        sb.append(minutes);
+        if (seconds < 10) {
+            sb.append("0");
+        }
+        sb.append(":");
+        sb.append(seconds);
+        return sb.toString();
+    }
 
 }
